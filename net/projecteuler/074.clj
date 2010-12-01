7794bc2b70bea0519f27fd61783544c55f8fa2c7
Guillaume Nargeot <killy971@gmail.com>
2010-12-02 00:08:27 +0900

    faster factorial function

--- net/projecteuler/074.clj
+++ net/projecteuler/074.clj
@@ -1,5 +1,17 @@
-(ns net.projecteuler.074
-  (:use [net.projecteuler.utils :only [fact]]))
+(ns net.projecteuler.074)
+
+(defn fact [digit]
+  (case digit
+    0 1
+    1 1
+    2 2
+    3 6
+    4 24
+    5 120
+    6 720
+    7 5040
+    8 40320
+    9 362880))
 
 (defn digits-fact [number]
   (loop [n number, res 0]
