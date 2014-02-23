9736a9607163537d1202a97a9a48e8d935b4b2a7
killy971 <killy971@gmail.com>
2014-02-23 23:07:55 +0900

    Modified solution to be more idiomatic

--- src/project_euler/problem_004.clj
+++ src/project_euler/problem_004.clj
@@ -4,10 +4,4 @@
 (defn solution-004
   "Returns the largest palindrome made from the product of two 3-digit numbers"
   []
-  (loop [a 100, b 100, max 0]
-    (let [n (* a b)]
-          (cond
-            (= 1000 a) max
-            (= 1000 b) (recur (inc a) (inc a) max)
-            (and (palindromic? n) (> n max)) (recur a (inc b) n)
-            :default (recur a (inc b) max)))))
+  (apply max (filter palindromic? (for [x (range 100 999) y (range 100 999) :when (>= y x)] (* x y)))))
