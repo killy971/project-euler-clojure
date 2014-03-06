93283882807dd89d8542b6b28f96f71fc0324d5a
killy971 <killy971@gmail.com>
2014-03-06 21:49:38 +0900

    Loop to reduce migration

--- src/project_euler/problem_032.clj
+++ src/project_euler/problem_032.clj
@@ -2,19 +2,19 @@
   (:require [project-euler.utils :refer [to-n]]
             [clojure.math.combinatorics :refer [permutations]]))
 
-(defn has-p? [mult prod]
-  (let [m1 (to-n (take 1 mult)) m4 (to-n (drop 1 mult))
-        m2 (to-n (take 2 mult)) m3 (to-n (drop 2 mult))]
+(defn has-p? [mult-d prod]
+  (let [m1 (to-n (take 1 mult-d)) m4 (to-n (drop 1 mult-d))
+        m2 (to-n (take 2 mult-d)) m3 (to-n (drop 2 mult-d))]
     (or (= (* m1 m4) prod) (= (* m2 m3) prod))))
 
-(def pand-prods
-  (loop [perm (permutations (range 1 10)), prods (set [])]
-    (if (empty? perm) prods
-      (let [digits (first perm)
-            mult (take 5 digits)
+(defn pand-prods []
+  (reduce
+    (fn [prods digits]
+      (let [mult-d (take 5 digits)
             prod (to-n (drop 5 digits))]
-        (recur (rest perm)
-               (if (has-p? mult prod) (conj prods prod) prods))))))
+        (if (has-p? mult-d prod) (conj prods prod) prods)))
+    (set [])
+    (permutations (range 1 10))))
 
 (defn solution-032 []
-  (reduce + pand-prods))
+  (reduce + (pand-prods)))
